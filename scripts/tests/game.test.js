7768a3a5147a0caa6905d135eac92cefb551c675
Kay-ddggxh <kathrin.welfare@gmail.com>
2022-09-02 15:08:23 +0000

    Create addTurn function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -5,9 +5,10 @@
 const {
     game,
     newGame,
-    showScore
+    showScore,
+    addTurn,
+    lightsOn
 } = require("../game");
-
 
 beforeAll(() => {
     let fs = require("fs");
@@ -52,7 +53,7 @@ describe("newGame works correctly", () => {
     test("should clear the player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
     });
-    test("should clear the computer sequence array", () => {
-        expect(game.currentGame.length).toBe(0);
+    test("should add one move to the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
     });
 });
